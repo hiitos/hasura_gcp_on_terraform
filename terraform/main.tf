e7344c2d4d59a73cbc421a33e95ce5e6c726ed52
hitose kakazu <hkakazu@hitosenoMacBook-Air.local>
2022-10-23 01:06:44 +0900

    cloudbuildテスト

--- terraform/main.tf
+++ terraform/main.tf
@@ -104,81 +104,100 @@ resource "google_sql_user" "users" {
 
 #### Cloud run ####
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/cloud_run_service
-resource "google_cloud_run_service" "defalut" {
-  # Cloud runの名前
-  name = "cloudrun-srv"
-  location = "asia-northeast1"
-  autogenerate_revision_name = true
+# resource "google_cloud_run_service" "defalut" {
+#   # Cloud runの名前
+#   name = "cloudrun-srv"
+#   location = "asia-northeast1"
+#   autogenerate_revision_name = true # 自動でリビジョン末尾の識別文字列を入れるために必要
 
-  template {
-    spec {
-      containers {
-        image = "us-docker.pkg.dev/cloudrun/container/hello"
-        env {
-          name  = "DB_NAME"
-          value = google_sql_database.database.name
-        }
-        env {
-          name  = "DB_USER"
-          value = google_sql_user.users.name
-        }
-        env {
-          name  = "DB_PASS"
-          value = google_sql_user.users.password
-        }
-        env {
-          name  = "INSTANCE_CONNECTION_NAME"
-          value = google_sql_database_instance.postgres13_harat_tok_line.connection_name
-        }
-        env {
-          name  = "DB_PORT"
-          value = "5432"
-        }
-        env {
-          name  = "INSTANCE_HOST"
-          value = google_sql_database_instance.postgres13_harat_tok_line.private_ip_address
-        }
-        env {
-          name  = "PRIVATE_IP"
-          value = "TRUE"
-        }
-        # env {
-        #   name  = "DB_IAM_USER"
-        #   value = "${google_service_account.github_actions.name}@${var.project}.iam"
-        # }
-      }
-    }
-    metadata {
-      # メタデータのキーマップ
-      annotations = {
-        "autoscaling.knative.dev/maxScale"      = "1000"
-        "run.googleapis.com/cloudsql-instances" = google_sql_database_instance.postgres13_harat_tok_line.connection_name
-        "run.googleapis.com/client-name"        = "terraform"
-        "run.googleapis.com/vpc-access-connector" = google_vpc_access_connector.vpc_connector.name
-        "run.googleapis.com/vpc-access-egress" = "all-traffic"
-        # "run.googleapis.com/vpc-access-egress"    = "private-ranges-only"
-      }
-    }
-  }
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
-}
+#   template {
+#     spec {
+#       containers {
+#         # image = "us-docker.pkg.dev/cloudrun/container/hello"
+#         image = "us.gcr.io/${var.project_id}/sample-api" # すでにimageがアップロード済みでないといけない
+#         env {
+#           name  = "DB_NAME"
+#           value = google_sql_database.database.name
+#         }
+#         env {
+#           name  = "DB_USER"
+#           value = google_sql_user.users.name
+#         }
+#         env {
+#           name  = "DB_PASS"
+#           value = google_sql_user.users.password
+#         }
+#         env {
+#           name  = "INSTANCE_CONNECTION_NAME"
+#           value = google_sql_database_instance.postgres13_harat_tok_line.connection_name
+#         }
+#         env {
+#           name  = "DB_PORT"
+#           value = "5432"
+#         }
+#         env {
+#           name  = "INSTANCE_HOST"
+#           value = google_sql_database_instance.postgres13_harat_tok_line.private_ip_address
+#         }
+#         env {
+#           name  = "PRIVATE_IP"
+#           value = "TRUE"
+#         }
+#         # env {
+#         #   name  = "DB_IAM_USER"
+#         #   value = "${google_service_account.github_actions.name}@${var.project}.iam"
+#         # }
+#       }
+#     }
+#     metadata {
+#       # メタデータのキーマップ
+#       annotations = {
+#         "autoscaling.knative.dev/maxScale"      = "1000"
+#         "run.googleapis.com/cloudsql-instances" = google_sql_database_instance.postgres13_harat_tok_line.connection_name
+#         "run.googleapis.com/client-name"        = "terraform"
+#         "run.googleapis.com/vpc-access-connector" = google_vpc_access_connector.vpc_connector.name
+#         "run.googleapis.com/vpc-access-egress" = "all-traffic"
+#         # "run.googleapis.com/vpc-access-egress"    = "private-ranges-only"
+#       }
+#     }
+#   }
+#   traffic {
+#     percent         = 100
+#     latest_revision = true
+#   }
+# }
 
-data "google_iam_policy" "noauth" {
-  binding {
-    role = "roles/run.invoker"
-    members = [
-      "allUsers",
-    ]
-  }
-}
+# data "google_iam_policy" "noauth" {
+#   binding {
+#     role = "roles/run.invoker"
+#     members = [
+#       "allUsers",
+#     ]
+#   }
+# }
 
-resource "google_cloud_run_service_iam_policy" "noauth" {
-  location    = google_cloud_run_service.defalut.location
-  project     = google_cloud_run_service.defalut.project
-  service     = google_cloud_run_service.defalut.name
+# resource "google_cloud_run_service_iam_policy" "noauth" {
+#   location    = google_cloud_run_service.defalut.location
+#   project     = google_cloud_run_service.defalut.project
+#   service     = google_cloud_run_service.defalut.name
 
-  policy_data = data.google_iam_policy.noauth.policy_data
-}
+#   policy_data = data.google_iam_policy.noauth.policy_data
+# }
+
+# # cloud build
+# resource "google_cloudbuild_trigger" "cloudbuild_api" {
+#   location = "asia-northeast1"
+#   name     = "cloudbuild-api"
+#   filename = "cloudbuild.yml"
+
+#   github {
+#     owner = "hiitos"
+#     name  = "hasura_gcp_on_terraform"
+#     push {
+#       branch = "^master$"
+#     }
+#   }
+#   included_files = [
+#     "Dockerfile",
+#   ]
+# }
